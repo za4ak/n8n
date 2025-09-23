99564efe470232b543026f70134555f3d63d4024
Danny Martini <danny@n8n.io>
2025-09-23 11:51:20 +0200

    remove uninformative comment

--- packages/core/src/execution-engine/workflow-execute.ts
+++ packages/core/src/execution-engine/workflow-execute.ts
@@ -1999,7 +1999,6 @@ export class WorkflowExecute {
 								while (nodeFailed && tryIndex !== maxTries - 1) {
 									await sleep(waitBetweenTries);
 
-									// retries for failure happen here
 									runNodeData = await this.runNode(
 										workflow,
 										executionData,
